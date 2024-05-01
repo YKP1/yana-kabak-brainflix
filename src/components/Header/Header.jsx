1aca66fdee647501a6d055d0da824cf2b09a2c82
YK <yanakabakova@gmail.com>
2024-04-30 21:45:47 -0400

    header draft complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
 import "./Header.scss";
+import brainFlixLogo from "../../assets/Logo/BrainFlix-logo.svg";
+import HeaderImage from "../../assets/images/Mohan-muruge.jpg";
 
 export default function Header() {
   return (
     <header className="header">
-      <nav className="header__links header__links--left">
-        <a href="/">Home</a> |<a href="/">About</a> |<a href="/">Contact</a>
-      </nav>
-      <div className="header__logo"></div>
-      <div className="header__links header__links--right">
-        <input className="search" placeholder="Search..." />
-        <p className="lastPressed"></p>
+
+      <div className="header__left">
+        <img src={brainFlixLogo} alt="" className="header__logo" />
       </div>
+
+      <div className="header__mid">
+        <input className="header__search" placeholder="Search" />
+        <img src={HeaderImage} alt="profile picture" className="header__image"/>
+      </div>
+
+      <div className="header__right">
+        <button className="header__button">UPLOAD</button>
+      </div>
+      
     </header>
   );
 }
+
 
