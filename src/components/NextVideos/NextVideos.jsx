a1eb2f314d29525c6a01503d05e6b89432feba9d
YK <yanakabakova@gmail.com>
2024-05-01 23:26:26 -0400

    developing videolist str, folder creations

---
+++ src/components/NextVideos/NextVideos.jsx
@@ -0,0 +1,11 @@
+import "./NextVideos.scss"
+
+export default function NextVideos() {
+    return (
+        <section className = "videoslist">
+            <p className = "videoslist__header"></p>
+        <div className = "videoslist__container">
+        </div>
+        </section>
+    );
+  }
