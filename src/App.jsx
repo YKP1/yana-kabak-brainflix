9eaab9200c1240cd53172dc686a2c8232fe8df6a
YK <yanakabakova@gmail.com>
2024-05-08 23:59:44 -0400

    re-structuring API's and functions to render properly - not rendering at the moment

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
-import videos from "./data/video-details.json";
 import { useEffect, useState } from "react";
 import VideoPage from "./pages/VideoPage/VideoPage";
 import UploadPage from "./pages/UploadPage/UploadPage";
 import axios from "axios";
 
 const BASE_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
-const apiKey = 'cec6b186-c74d-452f-bf87-1bbc245ccb46';
+const apiKey = "cec6b186-c74d-452f-bf87-1bbc245ccb46";
 
 function App() {
   const [selectedVideo, setSelectedVideo] = useState(null);
@@ -21,13 +20,8 @@ function App() {
     getVideos();
   }, []);
 
-  if (!getVideos){
-    return <div className = "loader" >Sholdn't take tooooooo lo...</div>
-  }
-
-  async function getVideos(){
-    const responsito = await axios.get(`${BASE_URL}videos/:id${apiKey}`);
-    setSelectedVideo(responsito.data);
+  if (!selectedVideo ) {
+    return <div className="loader">Shouldn't take tooooooo lo...</div>;
   }
 
   return (
@@ -44,11 +38,19 @@ function App() {
             />
           }
         />
-        <Route path="video/:id" element={ <VideoPage selectedVideo={selectedVideo}/>} />
+        <Route
+          path="/video/:id"
+          element={<VideoPage selectedVideo={selectedVideo} />}
+        />
         <Route path="/upload" element={<UploadPage />} />
       </Routes>
     </BrowserRouter>
   );
+
+  async function getVideos() {
+    const responsito = await axios.get(`${BASE_URL}videos?$api_key=${apiKey}`);
+    setSelectedVideo(responsito.data);
+  } 
 }
 
 export default App;
--- src/pages/VideoPage/VideoPage.jsx
+++ src/pages/VideoPage/VideoPage.jsx
@@ -5,28 +5,24 @@ import CommentsForm from "../../components/CommentsForm/CommentsForm";
 import Comments from "../../components/Comments/Comments";
 import NextVideos from "../../components/NextVideos/NextVideos";
 import { useEffect, useState } from "react";
+import axios from "axios";
 
 const BASE_URL = "https://unit-3-project-api-0a5620414506.herokuapp.com/";
-const apiKey = "cec6b186-c74d-452f-bf87-1bbc245ccb46";
+// const apiKey = "cec6b186-c74d-452f-bf87-1bbc245ccb46";
 
 const VideoPage = ({ selectedVideo, setSelectedVideo }) => {
+  const { id } = useParams();
 
-  const {id} = useParams(); 
-
-  const [videoid, setVideoId] = useState(null); 
+  const [videoid, setVideoId] = useState(null);
 
   useEffect(() => {
-    getVideoId ();
-  }, [id]);
+    getVideoId();
+  }, [id, setSelectedVideo]);
 
-  if (!videoid){
-    return <div className="loader">wait for it...</div>
+  if (!videoid) {
+    return <div className="loader">wait for it...</div>;
   }
 
-  async function getVideoId(){
-    const {data} = await axios.get (`${BASE_URL}/videos/:id/${id || ids[0]}`)
-    getVideoId(data);
-  }
   return (
     <>
       <VideoPlayer video={selectedVideo} />
@@ -45,6 +41,11 @@ const VideoPage = ({ selectedVideo, setSelectedVideo }) => {
       </div>
     </>
   );
+
+  async function getVideoId() {
+    const { data } = await axios.get(`${BASE_URL}/videos/:id/${id || ids[0]}`);
+    setSelectedVideo(data);
+  }
 };
 
 export default VideoPage;
