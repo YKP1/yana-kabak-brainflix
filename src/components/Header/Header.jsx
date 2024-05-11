1ee40f6971f9bb2e098e9fc9569e7457beaa1944
YK <yanakabakova@gmail.com>
2024-05-11 17:35:33 -0400

    BrainFlix logo linked to the home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,15 @@ import "./Header.scss";
 import brainFlixLogo from "../../assets/Logo/BrainFlix-logo.svg";
 import HeaderImage from "../../assets/images/Mohan-muruge.jpg";
 import UploadButton from "../UploadButton/UploadButton";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  
   return (
     <header className="header">
-        <img src={brainFlixLogo} alt="" className="header__logo" />
-
+      <Link to="/">
+        <img src={brainFlixLogo} alt="BrainFlix Logo" className="header__logo" />
+      </Link>
       <div className="header__search">
         <input className="header__search--textbox" placeholder="Search" />
         <img src={HeaderImage} alt="profile picture" className="header__image"/>
